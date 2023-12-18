50543d928186436207aea568bc6c365e0288268b
SPIDEYcharles <pritham30charles@gmail.com>
2023-12-18 09:25:17 +0530

    hi

---
+++ datatypes.java
@@ -0,0 +1,16 @@
+public class datatypes {
+
+	public static void main(String[] args) {
+		
+		double age;
+		age = 129.95444;
+		
+
+//		byte 10 < short 12 < int 17 < long 23 < float 30 < double 40
+		System.out.println("Hello Good Morning ! This is Dinesh and I am "+ age +" years old");
+		
+		char grade;
+		grade = 'A';
+		System.out.println("Hello Good Morning ! This is Dinesh and I got "+ grade +".");
+	}
+}
---
+++ hello.java
@@ -0,0 +1,14 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("            .................................................................");
+        System.out.println("            :   Name            : PRITHAM CHARLES VARDHANAPU                :");
+        System.out.println("            :   Place           : Rajahmundry                               :");
+        System.out.println("            :   Skils           : HTML,CSS,Javascript,blender,illustrations :");
+        System.out.println("            :   qualification   : B.tech/IT                                 :");
+        System.out.println("            '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''");
+        System.out.println(" ");
+        System.out.println(" ");
+    }
+}
